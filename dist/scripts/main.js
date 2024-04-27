e4a175ea904c118912a124717606f9a4ce533888
blazenkits <kchoe5010@gmail.com>
2024-04-28 03:09:56 +0900

    v1.1.2

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,14 +3,16 @@ import { world, system } from "@minecraft/server";
 
 // scripts/maintenance.ts
 var version_documentation = {
-  0: "    !tp <\uC0AC\uC6A9\uC790 \uC774\uB984> -> \uC0AC\uC6A9\uC790\uC5D0\uAC8C \uC21C\uAC04\uC774\uB3D9\uD568."
+  0: ["!tp <\uC0AC\uC6A9\uC790 \uC774\uB984> -> \uC0AC\uC6A9\uC790\uC5D0\uAC8C \uC21C\uAC04\uC774\uB3D9\uD568."],
+  1: ["!home -> \uC9D1\uC73C\uB85C tp"]
 };
 
 // scripts/main.ts
-var BP_VERSION = 0;
+var BP_VERSION = 1;
 var START_TICK = 100;
 var VERSION_CHECK_SCOREBOARD = "VERSION_CHECK_SCOREBOARD";
-var SERVER_NAME = "\uC7AC\uBC0C\uB294 \uB9C8\uD06C \uC11C\uBC84";
+var HOME_LOCATION_SCOREBOARD = "HOME_LOCATION_SCOREBOARD";
+var SERVER_NAME = "\uC11C\uBC84";
 system.run(mainSetup);
 world.afterEvents.chatSend.subscribe(onPlayerChatSend);
 world.afterEvents.playerSpawn.subscribe(onPlayerSpawn);
@@ -26,7 +28,7 @@ function onPlayerSpawn(eventData) {
   if (!eventData.initialSpawn) {
     return;
   }
-  eventData.player.sendMessage(`${eventData.player.name}\uB2D8, ${SERVER_NAME}\uC5D0 \uC811\uC18D\uD55C \uAC83\uC744 \uD658\uC601\uD569\uB2C8\uB2E4!`);
+  eventData.player.sendMessage(`${eventData.player.name}, ${SERVER_NAME}\uC5D0 \uC811\uC18D\uD55C \uAC83\uC744 \uD658\uC601\uD569\uB2C8\uB2E4!`);
   eventData.player.sendMessage(`\uD604\uC7AC\uC2DC\uAC04\uC740 ${world.getDay()}\uC77C ${Math.floor(world.getTimeOfDay() / 1e3)}\uC2DC ${Math.floor(world.getTimeOfDay() % 1e3 / 1e3 * 60)}\uBD84 \uC785\uB2C8\uB2E4.`);
   let joinedString = world.getAllPlayers().map((player) => player.name).join(", ");
   eventData.player.sendMessage("\uD604\uC7AC \uC811\uC18D\uC790\uB294 " + joinedString + "\uC785\uB2C8\uB2E4.");
@@ -46,10 +48,17 @@ function onPlayerSpawn(eventData) {
     }
   }
   vcScoreboard.setScore(eventData.player, BP_VERSION);
-  if (is_version_upgrade && BP_VERSION in version_documentation) {
-    eventData.player.sendMessage(`\uB370\uC774\uD130\uD329 \uBC84\uC804 1.0.${BP_VERSION}\uC5D0 \uCD94\uAC00\uB41C \uAE30\uB2A5\uB4E4: `);
-    eventData.player.sendMessage(version_documentation[BP_VERSION]);
+  if (is_version_upgrade) {
+    eventData.player.sendMessage(`====================================`);
+    eventData.player.sendMessage(`\uC11C\uBC84\uAC00 \uC5C5\uB370\uC774\uD2B8\uB418\uC5C8\uC2B5\uB2C8\uB2E4. (-> ${BP_VERSION})`);
+    if (BP_VERSION in version_documentation) {
+      eventData.player.sendMessage(`\uCD94\uAC00 \uB0B4\uC6A9:`);
+      for (let message of version_documentation[BP_VERSION]) {
+        eventData.player.sendMessage("    " + message);
+      }
+    }
   }
+  eventData.player.sendMessage(`====================================`);
 }
 function onPlayerChatSend(eventData) {
   let player = eventData.sender;
@@ -82,6 +91,43 @@ function onPlayerChatSend(eventData) {
       world.getAllPlayers().forEach((target) => {
         player.sendMessage("    " + target.name);
       });
+      return;
+    }
+    case "!sethome": {
+      if (player.dimension.id != "minecraft:overworld") {
+        player.sendMessage(`sethome > \uD648 \uC124\uC815\uC740 \uC624\uBC84\uC6D4\uB4DC\uC5D0\uC11C\uB9CC \uAC00\uB2A5\uD569\uB2C8\uB2E4.`);
+        return;
+      }
+      let hlScoreboard = world.scoreboard.getObjective(HOME_LOCATION_SCOREBOARD);
+      if (!hlScoreboard) {
+        hlScoreboard = world.scoreboard.addObjective(HOME_LOCATION_SCOREBOARD, "Home-Location");
+      }
+      hlScoreboard.setScore("x", player.location.x);
+      hlScoreboard.setScore("y", player.location.y);
+      hlScoreboard.setScore("z", player.location.z);
+      player.sendMessage(`sethome > \uC131\uACF5!`);
+      return;
+    }
+    case "!home": {
+      let hlScoreboard = world.scoreboard.getObjective(HOME_LOCATION_SCOREBOARD);
+      if (!hlScoreboard) {
+        player.sendMessage(`home > \uC9D1 \uC704\uCE58\uAC00 \uC124\uC815\uB418\uC9C0 \uC54A\uC558\uC2B5\uB2C8\uB2E4.`);
+        return;
+      }
+      let px = hlScoreboard.getScore("x");
+      let py = hlScoreboard.getScore("y");
+      let pz = hlScoreboard.getScore("z");
+      if (px != void 0 && py != void 0 && pz != void 0) {
+        let tpSuccess = player.tryTeleport({ x: px, y: py, z: pz }, { dimension: world.getDimension("minecraft:overworld") });
+        if (tpSuccess) {
+          player.sendMessage(`home > \uC9D1\uC73C\uB85C \uC18C\uD658\uB418\uC5C8\uC2B5\uB2C8\uB2E4.`);
+        } else {
+          player.sendMessage(`home > \uC9D1\uC740 \uD604\uC7AC \uC18C\uD658\uD560 \uC218 \uC788\uB294 \uC704\uCE58\uAC00 \uC544\uB2D9\uB2C8\uB2E4.`);
+        }
+        return;
+      }
+      player.sendMessage(`home > \uC624\uB958`);
+      return;
     }
   }
 }
--- scripts/main.ts
+++ scripts/main.ts
@@ -2,10 +2,11 @@ import { world, system, ChatSendAfterEvent, PlayerJoinAfterEvent, PlayerSpawnAft
 import { version_documentation } from "./maintenance";
 
 // Globals
-const BP_VERSION = 0;
+const BP_VERSION = 1;
 const START_TICK = 100;
 const VERSION_CHECK_SCOREBOARD = "VERSION_CHECK_SCOREBOARD"
-const SERVER_NAME = "재밌는 마크 서버"
+const HOME_LOCATION_SCOREBOARD = "HOME_LOCATION_SCOREBOARD"
+const SERVER_NAME = "서버"
 
 // Entry Points
 system.run(mainSetup);
@@ -26,7 +27,7 @@ function mainSetup() {
 // Player Spawn Hook
 function onPlayerSpawn(eventData: PlayerSpawnAfterEvent){
   if (!eventData.initialSpawn) {return;}
-  eventData.player.sendMessage(`${eventData.player.name}님, ${SERVER_NAME}에 접속한 것을 환영합니다!`)
+  eventData.player.sendMessage(`${eventData.player.name}, ${SERVER_NAME}에 접속한 것을 환영합니다!`)
   eventData.player.sendMessage(`현재시간은 ${world.getDay()}일 ${Math.floor(world.getTimeOfDay()/ 1000)}시 ${Math.floor((world.getTimeOfDay() % 1000) / 1000 * 60)}분 입니다.`)
   let joinedString = world.getAllPlayers().map(player => player.name).join(', ');
   eventData.player.sendMessage("현재 접속자는 " + joinedString +"입니다.");
@@ -51,11 +52,17 @@ function onPlayerSpawn(eventData: PlayerSpawnAfterEvent){
     }
   }
   vcScoreboard.setScore(eventData.player, BP_VERSION)
-
-  if (is_version_upgrade && (BP_VERSION in version_documentation)){
-    eventData.player.sendMessage(`데이터팩 버전 1.0.${BP_VERSION}에 추가된 기능들: `)
-    eventData.player.sendMessage(version_documentation[BP_VERSION])
+  if (is_version_upgrade){
+    eventData.player.sendMessage(`====================================`)
+    eventData.player.sendMessage(`서버가 업데이트되었습니다. (-> ${BP_VERSION})`)
+    if(BP_VERSION in version_documentation){
+      eventData.player.sendMessage(`추가 내용:`)
+      for (let message of version_documentation[BP_VERSION]){
+        eventData.player.sendMessage("    " + message as string)
+      }
+    }
   }
+  eventData.player.sendMessage(`====================================`)
 }
 
 function onPlayerChatSend(eventData: ChatSendAfterEvent){
@@ -91,6 +98,52 @@ function onPlayerChatSend(eventData: ChatSendAfterEvent){
       world.getAllPlayers().forEach(target => {
         player.sendMessage("    " + target.name);
       });
+      return
+    }
+
+    case "!sethome": {
+      if (player.dimension.id != "minecraft:overworld") {
+        player.sendMessage(`sethome > 홈 설정은 오버월드에서만 가능합니다.`)
+        return
+      }
+      let hlScoreboard = world.scoreboard.getObjective(HOME_LOCATION_SCOREBOARD)
+  
+      // Initialize scoreboards if not present.
+      if (!hlScoreboard){
+        hlScoreboard = world.scoreboard.addObjective(HOME_LOCATION_SCOREBOARD, "Home-Location")
+      }
+
+      hlScoreboard.setScore("x", player.location.x)
+      hlScoreboard.setScore("y", player.location.y)
+      hlScoreboard.setScore("z", player.location.z)
+      
+      player.sendMessage(`sethome > 성공!`)
+      return
+      
+    }
+
+    case "!home": {
+      let hlScoreboard = world.scoreboard.getObjective(HOME_LOCATION_SCOREBOARD)
+  
+      if (!hlScoreboard){
+        player.sendMessage(`home > 집 위치가 설정되지 않았습니다.`)
+        return
+      }
+      let px = hlScoreboard.getScore("x")
+      let py = hlScoreboard.getScore("y")
+      let pz = hlScoreboard.getScore("z")
+      if( px != undefined && py != undefined && pz != undefined){
+        let tpSuccess = player.tryTeleport({x: px, y: py, z: pz}, {dimension: world.getDimension("minecraft:overworld")})
+          if (tpSuccess){
+            player.sendMessage(`home > 집으로 소환되었습니다.`)
+          } else {
+            player.sendMessage(`home > 집은 현재 소환할 수 있는 위치가 아닙니다.`)
+          }
+          return
+      }
+
+      player.sendMessage(`home > 오류`)
+      return
     }
  
   }
